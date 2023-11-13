4730d530c6bdbea3fea5708dd46f361ec8bbbabc
Jim Katz <jimk@tbrc.org>
2023-11-13 16:44:35 -0500

    WIP. Add "Add Archival group"

--- src/test/java/io/bdrc/lib/RepoClientTest.java
+++ src/test/java/io/bdrc/lib/RepoClientTest.java
@@ -2,6 +2,7 @@ package io.bdrc.lib;
 
 import org.fcrepo.client.*;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -21,12 +22,17 @@ class RepoClientTest {
     private void createClient() {
         FcrepoClient.FcrepoClientBuilder fcb = new FcrepoClient.FcrepoClientBuilder();
         _fcrepoClient = fcb.build();
+
+        // This gnarl from stackoverflow. Slf4j doesn't seem to havea set level, but the underlying
+        // log4j does
+
+        _logger.error("diagnose {}","_logger");
     }
 
 
     @Test
     public void TestGet() throws URISyntaxException, RuntimeException {
-        GetBuilder blarg = _fcrepoClient.get(new URI("http://sattva:8080/rest/Volumes"));
+        GetBuilder blarg = _fcrepoClient.get(new URI("http://sattva:8080/rest"));
 
         // Get json
         try (FcrepoResponse top_level = blarg.accept("application/ld+json").perform()) {
@@ -49,18 +55,23 @@ class RepoClientTest {
         }
     }
 
+    @Disabled
     @Test
-    public void TestAddArchivalGroup() {
+    public void TestAddArchivalGroup() throws URISyntaxException , RuntimeException{
         // Blending the RESTFul HTTP API @ https://wiki.lyrasis.org/display/FEDORA6x/RESTful+HTTP+API+-+Containers#RESTfulHTTPAPIContainers-BluePOSTCreatenewresourceswithinaLDPcontainer
         // and the FcrepoClient API @https://github.com/fcrepo-exts/fcrepo-java-client
         // curl -X POST -u fedoraAdmin:fedoraAdmin -H "Slug: my-archival-group" -H "Link: <http://fedora.info/definitions/v4/repository#ArchivalGroup>;rel=\"type\"" http://localhost:8080/rest
         PostBuilder postBuilder = _fcrepoClient.post(new URI("http://sattva:8080/rest"));
         postBuilder.addHeader("Slug", "Volumes");
-        postBuilder.addHeader("Link", "Link: <http://fedora.info/definitions/v4/repository#ArchivalGroup>;rel=\"type\"");
+        postBuilder.addHeader("Link", "<http://fedora.info/definitions/v4/repository#ArchivalGroup>;rel=\"type\"");
 
-        try (FcrepoResponse response = new postBuilder.perform()) {
+        try (FcrepoResponse response = postBuilder.perform()) {
             URI location = response.getLocation();
             _logger.info("Container creation status and location: {}, {}", response.getStatusCode(), location);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (FcrepoOperationFailedException e) {
+            throw new RuntimeException(e);
         }
     }
 
