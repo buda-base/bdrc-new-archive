e59a19400fc9accb5bffd796c30a12c6c14991ef
Jim Katz <jimk@tbrc.org>
2024-01-25 14:20:08 -0500

    Add a Version to a resource

--- src/main/java/io/bdrc/lib/BdrcRepoClient.java
+++ src/main/java/io/bdrc/lib/BdrcRepoClient.java
@@ -1,9 +1,6 @@
 package io.bdrc.lib;
 
-import org.fcrepo.client.FcrepoClient;
-import org.fcrepo.client.FcrepoOperationFailedException;
-import org.fcrepo.client.FcrepoResponse;
-import org.fcrepo.client.GetBuilder;
+import org.fcrepo.client.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,6 +8,7 @@ import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URI;
+import java.net.URISyntaxException;
 
 
 // Facade for FcrepoClient, uses the builder.
@@ -118,15 +116,15 @@ public class BdrcRepoClient {
 
 
     /**
-     * @param parent parent container
-     * @param container  container type
-     * @param title    Dublin Core title - recommend to not use in FcRepo environment
+     * @param parent      parent container
+     * @param container   container type
+     * @param title       Dublin Core title - recommend to not use in FcRepo environment
      * @param description - free text description of object
-     * @return  URI of newly created container
+     * @return URI of newly created container
      * @throws RuntimeException on any exception
      */
     public String AddContainerDCTitleDesc(String parent, LdpContainer container, String title,
-                               String description) throws RuntimeException
+                                          String description) throws RuntimeException
     {
 
         // Set the content type for the direct container
@@ -134,7 +132,7 @@ public class BdrcRepoClient {
 
         // Create the container
         URI containerUri = getEndpoint();
-        if (parent != null ) {
+        if (parent != null) {
             containerUri = getEndpoint().resolve(parent + "/");
         }
 
@@ -158,7 +156,7 @@ public class BdrcRepoClient {
 
         String respbody;
 
-        _logger.info(container.toString()+"; rel=\"type\"");
+        _logger.info(container.toString() + "; rel=\"type\"");
         try (FcrepoResponse top_level = _fcrepoClient
                 .post(containerUri)
 
@@ -174,7 +172,7 @@ public class BdrcRepoClient {
                 // Take 1 - add type as header
                 // .addHeader("Link", "<http://www.w3.org/ns/ldp#DirectContainer>; rel=\"type\"")
                 // Take 2 - use the passed in container type
-                .addHeader("Link", container +"; rel=\"type\"")
+                .addHeader("Link", container + "; rel=\"type\"")
                 .perform()) {
             if (top_level.getStatusCode() != 201) {
                 throw new RuntimeException(String.format("Add Container  %s/%s failed: %d", containerUri, (title == null) ? "" : title,
@@ -190,7 +188,7 @@ public class BdrcRepoClient {
     }
 
     public String AddContainerSlug(String parent, LdpContainer container, String title,
-                               String description) throws RuntimeException
+                                   String description) throws RuntimeException
     {
 
         // Set the content type for the direct container
@@ -198,47 +196,26 @@ public class BdrcRepoClient {
 
         // Create the container
         URI containerUri = getEndpoint();
-        if (parent != null ) {
+        if (parent != null) {
             containerUri = getEndpoint().resolve(parent + "/");
         }
 
-        // Add dublin core title and description to the container
-        // Callers should not, in general, use dc:title
-        // See https://www.dublincore.org/specifications/dublin-core/dcmi-terms/#section-3
-        StringBuilder dcTTL = new StringBuilder();
-        if (title != null) {
-            dcTTL.append(String.format("@prefix dc: <http://purl.org/dc/elements/1.1/> <> dc:title \"%s\" .\n",
-                    title));
-        }
-        if (description != null) {
-            dcTTL.append(String.format("@prefix dc: <http://purl.org/dc/elements/1.1/> <> dc:description \"%s\" .\n",
-                    description));
-        }
-        if ((description == null) && (title == null)) {
-            dcTTL.append("a <> .\n");
-        }
-
-        InputStream containerBodyStream = getInputStream(dcTTL.toString());
-
         String respbody;
+        String containerType =container.toString() + "; rel=\"type\"";
+        _logger.debug("Container type: {}  URI: {}", containerType, containerUri);
 
-        _logger.info(container.toString()+"; rel=\"type\"");
         try (FcrepoResponse top_level = _fcrepoClient
                 .post(containerUri)
 
-                // UIse the header to define the content type, not the ttl - the sample
-                // from the FcrepoClient documentation is wrong - gives a 400
-                .body(containerBodyStream, contentType)
-
                 // Take 1aProblem with a slug is that fcrepo will create a new resource if it has the Slug
                 // as an existing one.
-                // jimk: Don't add "slug" header, let fcrepo name its resource
+                //
                 .addHeader("Slug", title)
 
                 // Take 1 - add type as header
                 // .addHeader("Link", "<http://www.w3.org/ns/ldp#DirectContainer>; rel=\"type\"")
                 // Take 2 - use the passed in container type
-                .addHeader("Link", container +"; rel=\"type\"")
+                .addHeader("Link", containerType)
                 .perform()) {
             if (top_level.getStatusCode() != 201) {
                 throw new RuntimeException(String.format("Add Container  %s/%s failed: %d", containerUri, (title == null) ? "" : title,
@@ -252,6 +229,7 @@ public class BdrcRepoClient {
         return respbody;
 
     }
+
     private static InputStream getInputStream(String content) {
 
         // +1 OpenAI
@@ -260,7 +238,8 @@ public class BdrcRepoClient {
 
     public String AddBinary(final String parentUrl, final java.nio.file.Path input_file,
                             final String file_content_type,
-                            final String md5sum) {
+                            final String md5sum)
+    {
         String inputFileName = input_file.getFileName().toString();
         // Maybe this was the error:
         // URI containerUri = getEndpoint().resolve(parentUrl + "/" + inputFileName);
@@ -284,5 +263,123 @@ public class BdrcRepoClient {
             throw new RuntimeException(e);
         }
         return respbody;
+    }
+
+    /**
+     * Increment
+     * @param resourceName
+     * How would I add the "message" key, below?
+     * @return
+     *    "v2": {
+     *       "created": "2018-02-02T02:02:02Z",
+     *       "message": "Fix bar.xml, remove image.tiff, add empty2.txt, rename file1.txt to file2.txt, add updated file1.txt",
+     *       "state": {
+     *         "9bb43j...n3a": [ "file2.txt" ],
+     *         "u8b99v...7b2": [ "file1.txt" ],
+     *         "4d27c8...b53": [ "foo/bar.xml" ],
+     *         "cf83e1...a3e": [ "empty.txt", "empty2.txt" ]
+     *       },
+     */
+    public URI AddVersion(final String resourceName, String messageSlug) throws BdrcRepoException  {
+        // https://github.com/fcrepo-exts/fcrepo-java-client
+        // https://wiki.lyrasis.org/display/FEDORA4x/Versioning
+
+        URI versionLocation ;
+        String _resourceName = resourceName;
+        try {
+            if (!_resourceName.endsWith("/")) {
+                _resourceName += "/";
+            }
+        URI versionURI = addChild(_endpoint, _resourceName);
+        versionURI = addChild(versionURI, "fcr:versions");
+        FcrepoResponse response = new PostBuilder(versionURI, _fcrepoClient)
+                .perform();
+            versionLocation = response.getLocation();
+            _logger.debug("Version creation input: {}  status and location: {}, {}", versionURI, response.getStatusCode(),
+                    versionLocation);
+        }
+        catch (URISyntaxException | FcrepoOperationFailedException e ){
+            throw new BdrcRepoException(e.getMessage());
+        }
+
+        return versionLocation ;
+
+    }
+
+    /**
+     * Work through examples in https://github.com/fcrepo-exts/fcrepo-java-client
+     * That doc is a loser. The Lyrasis Wiki shows how to use transactions:
+     * https://wiki.lyrasis.org/display/FEDORA4x/Transactions
+     */
+
+    public URI txnBegin() throws BdrcRepoException {
+        URI txnLocation;
+        try {
+            URI _tmp = addChild(_endpoint, "fcr:tx");
+            FcrepoResponse response = _fcrepoClient.post(_tmp).perform();
+            txnLocation = response.getLocation();
+            _logger.debug("Transaction creation status and location: {}, {}", response.getStatusCode(), txnLocation);
+        } catch (Exception e) {
+            throw (BdrcRepoException) (e);
+        }
+        return txnLocation;
+    }
+
+    /**
+     * Roll back and fcRepo Transction
+     *
+     * @param txnLocation fcRepo transactions are identified by an URI
+     */
+    public void txnRollback(URI txnLocation) throws BdrcRepoException {
+        txnOp(txnLocation, "fcr:rollback");
+    }
+
+    /**
+     * Commit an fcRepo transaction
+     *
+     * @param txnLocation fcRepo transactions are identified by an URI
+     */
+    public void txnCommit(URI txnLocation) throws BdrcRepoException {
+        txnOp(txnLocation, "fcr:commit");
+    }
+
+    private void txnOp(URI txnLocation, String op) throws BdrcRepoException {
+        try {
+            URI commitURI = addChild(txnLocation, "./" + op);
+            FcrepoResponse response = new PostBuilder(commitURI, _fcrepoClient).perform();
+            _logger.debug("Transaction commit status: {}", response.getStatusCode());
+        }
+        catch (FcrepoOperationFailedException | URISyntaxException e ) {
+            throw new BdrcRepoException(e.getMessage());
+        }
+    }
+
+
+    public URI addChild(URI baseURI, String extension) throws URISyntaxException {
+        // Extract existing components
+
+        return baseURI.resolve("./" + extension);
+       //  return URI.create(baseURI.toString()  + extension + "/") ;
+        // Create a new URI with the extended components
+        // return new URI(scheme, null, host, port, path, extension, null);
+    }
+
+    public URI addChild(URI baseURI, String paramName, String paramValue) throws URISyntaxException {
+        // Extract existing components
+        String scheme = baseURI.getScheme();
+        String host = baseURI.getHost();
+        int port = baseURI.getPort();
+        String path = baseURI.getPath();
+
+        // Create a query string with the new parameter
+        String query = _endpoint.getQuery();
+        if (query == null) {
+            query = paramName + "=" + paramValue;
+        } else {
+            query += "&" + paramName + "=" + paramValue;
+        }
+
+        // Create a new URI with the extended components
+        return new URI(scheme, null, host, port, path, query, null);
     }
 }
--- src/main/java/io/bdrc/lib/Main.java
+++ src/main/java/io/bdrc/lib/Main.java
@@ -26,11 +26,11 @@ public class Main {
         return ArchivalGroupFactory.create(fcRestEndpoint, agName, agDescription);
     }
 
-    public static void CreateVolumes( BdrcRepoClient client, ArchivalGroup archivalGroup, String[] volumeNames) {
+    public static void AddVolumes(BdrcRepoClient client, String resourceParent, String[] volumeNames) {
         // Add a direct container to the Volumes archival group
 
         for (String volumeName : volumeNames) {
-            String volume = client.AddContainerSlug(archivalGroup.getName(), LdpContainer.DirectContainer, volumeName,
+            String volume = client.AddContainerSlug(resourceParent, LdpContainer.DirectContainer, volumeName,
                     "Programmatically created");
             _logger.debug(volume);
         }
@@ -71,7 +71,7 @@ public class Main {
                         try {
                             String md5sum = calculateMD5(filePath);
                             String contentType = Files.probeContentType(filePath);
-                            _logger.debug(" contentType {} " , contentType);
+                            _logger.debug(" contentType {} ", contentType);
                             String image = client.AddBinary(parentUrl, filePath, contentType, md5sum);
                             _logger.debug(image);
 
@@ -88,7 +88,7 @@ public class Main {
     }
 
 
-    public static void main(String[] args) throws URISyntaxException {
+    public static void main(String[] args) throws URISyntaxException, BdrcRepoException {
 
         // Add an Archival group to the repository
 
@@ -96,7 +96,7 @@ public class Main {
         // In each container, add three image files.
 
         URI fcRestEndpoint = new URI("http://localhost:8088/fcrepo/rest/");
-        BdrcRepoClient client = new BdrcRepoClient(fcRestEndpoint,"application/n-triples");
+        BdrcRepoClient client = new BdrcRepoClient(fcRestEndpoint, "application/n-triples");
         // BdrcRepoClient client = new BdrcRepoClient(fcRestEndpoint, "text/turtle");
 
         // Scenario 2: Add an Archival group, named "volumes"
@@ -104,22 +104,27 @@ public class Main {
         // Add three images to each container.
         // In between, monitor the repository with the Fedora UI
 
+        // Scenario 1 - get the Volumes resource
 //        String whatapp = client.GetResource("Volumes");
 //        _logger.debug("whatapp {}", whatapp);
-        String whatapp2 = client.GetResource("");
-        _logger.debug("whatapp2 {}", whatapp2);
-//        ArchivalGroup volumesAG = AddArchiveGroup(fcRestEndpoint, "Volumes",
+
+// Scenario 2 - get the root resources
+//        String whatapp2 = client.GetResource("");
+//        _logger.debug("whatapp2 {}", whatapp2);
+
+// Scenario 3 - create Volumes Archive group
+// ArchivalGroup volumesAG = AddArchiveGroup(fcRestEndpoint, "Volumes",
 //                "Archival Group of Image Volumes. Code created");
 //        _logger.debug("Created {} ", volumesAG);
 
+// Scenario 4 - Create Volumes in the Volumes archival group
 //        ArchivalGroup ag = ArchivalGroupFactory.get(client,"Volumes");
-         CreateVolumes(client, new ArchivalGroup("Volumes",null,null), new String[] {"Volume1", "Volume2",
-         "Volume3"});
+//        CreateVolumes(client, new ArchivalGroup("Volumes", null, null), new String[]{"Volume1", "Volume2",
+//                "Volume3"});
 
 
-
-        // Scenario 2: Get the volumes by name - this requires  a search on dc.title, or some other way
-        // to add a resource by name
+// Scenario 5: Get the volumes by name - this requires  a search on dc.title, or some other way
+// to add a resource by name
 //        String VolsArchiveGroup = client.GetResource("Volumes");
 //        _logger.info(VolsArchiveGroup);
 //        for (int i = 1 ; i <= 3; i++) {
@@ -127,9 +132,12 @@ public class Main {
 //            _logger.info(volumei);
 //        }
 
-        // This is the URL of an anonymous container
-	//  String imagesContainer = "http://sattva:8080/rest/76369565-4478-40a6-9e64-d236590a27b7";
-        // _logger.debug("Using existing {} ", imagesContainer);
+        // Scenario 6: Create a transaction, add some images to a Volume, commit.
+
+        //  test 1 - running under debugger, txn timed out. But was wrong approach anyway.
+        // TxnTest(client, "Volumes/Volume1", "/Users/jimk/dev/tmp/Projects/fcrepo-sandbox/sample-images"
+        // TxnTest(client, "Volumes/Volume2", "/Users/jimk/dev/tmp/Projects/fcrepo-sandbox/sample-images2");
+
 
         // Add an unnamed container to the Volumes archival group
         // client.AddContainer("Volumes", LdpContainer.DirectContainer,null,"W12345-I54321");
@@ -143,6 +151,69 @@ public class Main {
 //        Path source_dir = Paths.get("/Users/jimk/dev/tmp/Projects/fcrepo-sandbox/sample-images");
 //        AddImages(new_container, client, source_dir);
 
+        // Scenario 7: runs against an fcreo-docker with autoversioning off
+        // Note we don't use the same images as before, as fcrepo wants to de-duplicate.
+        //AddImages("Volumes/Volume3",client, Paths.get("/Users/jimk/dev/tmp/Projects/fcrepo-sandbox/sample-images3"));
 
+        // URI versionURI = client.AddVersion("Volumes/Volume3/", "Your Message Here - Slug");
+
+        // ****AAARRGH
+        //❯ curl -X POST -H "Accept: text/turtle" -u fedoraAdmin:fedoraAdmin http://localhost:8088/fcrepo/rest/Volumes/Volume3/fcr:versions
+        //Resource <info:fedora/Volumes/Volume3> is contained in Archival Group <Optional[info:fedora/Volumes]> and cannot be versioned directly. Version the Archival Group instead.%
+
+        // Scenario 8. Create a new Direct Container.
+        // add DirectContainers to it
+        // Add images to one of the direct containers
+        // Version the container.
+        String NagRoot = "Non_ArchivalGroup_Volumes";
+        String[] NagVolumes = {"NAG_Volume1", "NAG_Volume2", "NAG_Volume3"};
+
+
+        //      Add the root volume
+//        client.AddContainerSlug(null, LdpContainer.DirectContainer,NagRoot,
+//                "Programmatically created");
+//        AddVolumes(client,NagRoot, NagVolumes);
+//        String curVolume = String.format("%s/%s", NagRoot, NagVolumes[0]);
+//
+//        AddImages(curVolume, client, Paths.get("/Users/jimk/dev/tmp/Projects" +
+//                "/fcrepo-sandbox/sample-images3"));
+        URI versionURI = client.AddVersion(String.format("%s/%s", NagRoot, NagVolumes[2]), "Your Message Here - Slug");
+
+
+    }
+
+    /**
+     * This approach needs redesign. The transaction id is supposed to be inserted in the
+     * resource url:  http://yourHost:port/fcrepo/rest/fcr:tx/895dac11-2636-4807-b6de-f3ae5bfc6d3e/ + resource
+     * And also, this was not the way to inhibit versioning. You have to run the server with
+     * autoversioning disabled.
+     *
+     * @param client         BdrcRepoclient
+     * @param parentResource String representing container
+     * @param imageSource    directory containing images
+     * @throws BdrcRepoException
+     */
+    public static <parentResource> void TxnTest(BdrcRepoClient client, String parentResource, String imageSource) throws BdrcRepoException {
+        URI txnLocation = null;
+        boolean txnFail = false;
+
+        // leftover from debugging
+        // client.txnRollback(URI.create("http://localhost:8088/fcrepo/rest/fcr:tx/895dac11-2636-4807-b6de-f3ae5bfc6d3e/"));
+        try {
+            txnLocation = client.txnBegin();
+            String pc = client.getEndpoint() + parentResource;
+            AddImages(pc, client, Paths.get(imageSource));
+        } catch (BdrcRepoException e) {
+            txnFail = true;
+            _logger.error(e.getLocalizedMessage());
+        } finally {
+            if (txnFail) {
+                // Rollback the transaction
+                client.txnRollback(txnLocation);
+            } else {
+                // Commit the transaction
+                client.txnCommit(txnLocation);
+            }
+        }
     }
 }
