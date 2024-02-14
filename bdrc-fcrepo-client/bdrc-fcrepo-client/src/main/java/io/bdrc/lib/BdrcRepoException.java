f5e921b071dda9b02cac1f39e15a16128c5edf2f
Jim Katz <jimk@tbrc.org>
2024-02-14 11:09:49 -0500

    reorg 2 for modules

--- bdrc-fcrepo-client/bdrc-fcrepo-client/src/main/java/io/bdrc/lib/BdrcRepoException.java
+++
@@ -1,7 +0,0 @@
-package io.bdrc.lib;
-
-public class BdrcRepoException extends RuntimeException{
-    public BdrcRepoException(String message) {
-        super(message);
-    }
-}
--- bdrc-fcrepo-client/bdrc-fcrepo-client/src/main/java/module-info.java
+++
@@ -1,6 +0,0 @@
-module io.bdrc.lib {
-    exports io.bdrc.lib;
-    requires fcrepo.java.client;
-    requires org.apache.commons.lang3;
-    requires org.slf4j;
-}
